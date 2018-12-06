46c246b69ac859302e913dcfb7c7a718892fc7de
nasben <nasben8@gmail.com>
2018-12-06 01:35:46 +0000

    all

---
+++ tests/activity_tests.py
@@ -0,0 +1,127 @@
+import unittest
+
+import mock
+import redis
+
+import baseplate.context.redis
+
+import verbify_service_activity as activity
+from verbify_service_activity.counter import ActivityCounter
+from verbify_service_activity.activity_thrift import ActivityService
+
+
+class ActivityInfoTests(unittest.TestCase):
+    def test_always_fuzzed(self):
+        with mock.patch("random.randint") as randint:
+            randint.return_value = 3
+            info = activity.ActivityInfo.from_count(count=99)
+        self.assertEqual(info.count, 102)
+        self.assertTrue(info.is_fuzzed)
+
+    def test_range_of_fuzzing(self):
+        samples = [activity.ActivityInfo.from_count(count=10).count
+                   for i in range(1000)]
+        self.assertTrue(all(sample >= 10 for sample in samples))
+        self.assertTrue(all(sample <= 14 for sample in samples))
+        self.assertTrue(any(sample != 10 for sample in samples))
+
+        samples = [activity.ActivityInfo.from_count(count=1000).count
+                   for i in range(1000)]
+        self.assertTrue(all(sample >= 1000 for sample in samples))
+        self.assertTrue(all(sample <= 1005 for sample in samples))
+        self.assertTrue(any(sample != 1000 for sample in samples))
+
+    def test_json_roundtrip(self):
+        info = activity.ActivityInfo(count=42, is_fuzzed=True)
+
+        serialized = info.to_json()
+        deserialized = activity.ActivityInfo.from_json(serialized)
+
+        self.assertEqual(deserialized.count, 42)
+        self.assertTrue(deserialized.is_fuzzed)
+
+
+class MockActivityInfo(activity.ActivityInfo):
+    def to_json(self):
+        return (self.count, self.is_fuzzed)
+
+
+class ActivityServiceTests(unittest.TestCase):
+    def setUp(self):
+        self.mock_counter = mock.Mock(spec=ActivityCounter)
+        self.handler = activity.Handler(counter=self.mock_counter)
+        self.mock_context = mock.Mock()
+
+        redis_ = mock.Mock(spec=redis.StrictRedis)
+        pipeline = redis_.pipeline.return_value = mock.MagicMock(
+            spec=baseplate.context.redis.MonitoredRedisPipeline)
+        self.mock_context.redis = redis_
+        self.mock_pipe = pipeline.__enter__.return_value
+
+    def test_health_check(self):
+        self.handler.is_healthy(self.mock_context)
+        self.mock_context.redis.ping.assert_called_with()
+
+    def test_record_activity(self):
+        self.handler.record_activity(self.mock_context, "context", "visitor")
+
+        self.mock_counter.record_activity.assert_called_with(
+            self.mock_context.redis, "context", "visitor")
+
+    def test_record_activity_bad_id(self):
+        self.handler.record_activity(self.mock_context, u"\u2603", "visitor")
+        self.assertFalse(self.mock_counter.record_activity.called)
+
+        self.handler.record_activity(self.mock_context, "context", u"\u2603")
+        self.assertFalse(self.mock_counter.record_activity.called)
+
+    def test_count_activity_bad_id(self):
+        with self.assertRaises(ActivityService.InvalidContextIDException):
+            self.handler.count_activity(self.mock_context, u"\u2603")
+
+    @mock.patch("verbify_service_activity.ActivityInfo", autospec=True)
+    def test_count_activity_cache_hit(self, MockActivityInfo):
+        self.mock_context.redis.mget.return_value = ["serialized"]
+        deserialized = MockActivityInfo.from_json.return_value
+        deserialized.count = 33
+        deserialized.is_fuzzed = True
+
+        result = self.handler.count_activity(self.mock_context, "context")
+
+        MockActivityInfo.from_json.assert_called_with("serialized")
+        self.assertEqual(result.count, 33)
+        self.assertTrue(result.is_fuzzed)
+
+    @mock.patch("verbify_service_activity.ActivityInfo", new=MockActivityInfo)
+    def test_count_activity_cache_miss(self):
+        self.mock_context.redis.mget.return_value = [None]
+
+        self.mock_pipe.execute.return_value = [125]
+        with mock.patch("random.randint") as randint:
+            randint.return_value = 3
+            result = self.handler.count_activity(self.mock_context, "context")
+
+        self.mock_context.redis.mget.assert_called_with(["context/cached"])
+        self.assertEqual(result.count, 128)
+        self.assertTrue(result.is_fuzzed)
+
+        # 30 is how long we cache for.
+        self.mock_pipe.setex.assert_called_with("context/cached", 30, (128, True))
+
+    @mock.patch("verbify_service_activity.ActivityInfo", new=MockActivityInfo)
+    def test_count_activity_multi_cache_miss(self):
+        self.mock_context.redis.mget.return_value = [None, None]
+        self.mock_pipe.execute.return_value = [500, 600]
+
+        with mock.patch("random.randint") as randint:
+            randint.return_value = 0
+            self.handler.count_activity_multi(self.mock_context, ["one", "two"])
+
+        self.mock_pipe.setex.assert_has_calls([
+            mock.call("one/cached", 30, (500, True)),
+            mock.call("two/cached", 30, (600, True)),
+        ], any_order=True)
+
+    def test_count_activity_multi_empty_list(self):
+        result = self.handler.count_activity_multi(self.mock_context, [])
+        self.assertEqual(result, {})
---
+++ verbify_service_activity/__init__.py
@@ -0,0 +1,149 @@
+import json
+import logging
+import math
+import random
+import re
+
+import redis
+
+from baseplate import (
+    Baseplate,
+    config,
+    error_reporter_from_config,
+    metrics_client_from_config,
+    tracing_client_from_config,
+)
+from baseplate.context.redis import RedisContextFactory
+from baseplate.integration.thrift import BaseplateProcessorEventHandler
+
+from .activity_thrift import ActivityService, ttypes
+from .counter import ActivityCounter
+
+
+logger = logging.getLogger(__name__)
+_ID_RE = re.compile("^[A-Za-z0-9_]{,50}$")
+_CACHE_TIME = 30  # seconds
+
+
+class ActivityInfo(ttypes.ActivityInfo):
+    @classmethod
+    def from_count(cls, count):
+        # keep a minimum jitter range of 5 for counts over 100
+        decay = math.exp(float(-min(count, 100)) / 60)
+        jitter = round(5 * decay)
+        return cls(count=count + random.randint(0, jitter), is_fuzzed=True)
+
+    def to_json(self):
+        return json.dumps(
+            {"count": self.count, "is_fuzzed": self.is_fuzzed},
+            sort_keys=True,
+        )
+
+    @classmethod
+    def from_json(cls, value):
+        deserialized = json.loads(value)
+        return cls(
+            count=deserialized["count"],
+            is_fuzzed=deserialized["is_fuzzed"],
+        )
+
+
+class Handler(ActivityService.ContextIface):
+    def __init__(self, counter):
+        self.counter = counter
+
+    def is_healthy(self, context):
+        return context.redis.ping()
+
+    def record_activity(self, context, context_id, visitor_id):
+        if not _ID_RE.match(context_id) or not _ID_RE.match(visitor_id):
+            return
+
+        self.counter.record_activity(context.redis, context_id, visitor_id)
+
+    def count_activity(self, context, context_id):
+        results = self.count_activity_multi(context, [context_id])
+        return results[context_id]
+
+    def count_activity_multi(self, context, context_ids):
+        if not context_ids:
+            return {}
+
+        if not all(_ID_RE.match(context_id) for context_id in context_ids):
+            raise ActivityService.InvalidContextIDException
+
+        activity = {}
+
+        # read cached activity
+        cache_keys = [context_id + "/cached" for context_id in context_ids]
+        cached_info = context.redis.mget(cache_keys)
+        for context_id, cached_value in zip(context_ids, cached_info):
+            if cached_value is None:
+                continue
+            activity[context_id] = ActivityInfo.from_json(cached_value.decode())
+
+        # count any ones that were not cached
+        missing_ids = [id_ for id_ in context_ids if id_ not in activity]
+        if not missing_ids:
+            return activity
+
+        with context.redis.pipeline("count", transaction=False) as pipe:
+            for context_id in missing_ids:
+                self.counter.count_activity(pipe, context_id)
+            counts = pipe.execute()
+
+        # update the cache with the ones we just counted
+        to_cache = {}
+        for context_id, count in zip(missing_ids, counts):
+            if count is not None:
+                info = ActivityInfo.from_count(count)
+                to_cache[context_id] = info
+        activity.update(to_cache)
+
+        if to_cache:
+            with context.redis.pipeline("cache", transaction=False) as pipe:
+                for context_id, info in to_cache.items():
+                    pipe.setex(context_id + "/cached", _CACHE_TIME, info.to_json())
+                pipe.execute()
+
+        return activity
+
+
+def make_processor(app_config):  # pragma: nocover
+    cfg = config.parse_config(app_config, {
+        "activity": {
+            "window": config.Timespan,
+        },
+        "tracing": {
+            "endpoint": config.Optional(config.Endpoint),
+            "service_name": config.String,
+        },
+        "redis": {
+            "url": config.String,
+            "max_connections": config.Optional(config.Integer, default=100),
+        },
+    })
+
+    metrics_client = metrics_client_from_config(app_config)
+    tracing_client = tracing_client_from_config(app_config)
+    error_reporter = error_reporter_from_config(app_config, __name__)
+    redis_pool = redis.BlockingConnectionPool.from_url(
+        cfg.redis.url,
+        max_connections=cfg.redis.max_connections,
+        timeout=0.1,
+    )
+
+    baseplate = Baseplate()
+    baseplate.configure_logging()
+    baseplate.configure_metrics(metrics_client)
+    baseplate.configure_tracing(tracing_client)
+    baseplate.configure_error_reporting(error_reporter)
+    baseplate.add_to_context("redis", RedisContextFactory(redis_pool))
+
+    counter = ActivityCounter(cfg.activity.window.total_seconds())
+    handler = Handler(counter=counter)
+    processor = ActivityService.ContextProcessor(handler)
+    event_handler = BaseplateProcessorEventHandler(logger, baseplate)
+    processor.setEventHandler(event_handler)
+
+    return processor
